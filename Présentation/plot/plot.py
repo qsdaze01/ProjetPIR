a1b3d1714b0df8d364189e900f0246f357e66061
Dvd-Luc <Dvd-Luc>
2021-06-14 23:01:22 +0200

    amélioration plot

--- Présentation/plot/plot.py
+++ Présentation/plot/plot.py
@@ -7,24 +7,24 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
 
-x = range(1000)
+x = range(1200)
 val_read = [];
 with open(sys.argv[1], 'r') as f:
     d = f.readlines()
     for i in d:
         val_read.append((int)(i)) 
 
-title = 'Heroin Overdoses'	
+title = 'Frequence'	
 plot = pd.DataFrame(val_read,x)
 #XN,YN = augment(x,y,10)
 #augmented = pd.DataFrame(YN,XN)
 plot.columns = {title}
 
-Writer = animation.writers['ffmpeg']
-writer = Writer(fps=20, metadata=dict(artist='Me'), bitrate=1800)
+#Writer = animation.writers['ffmpeg']
+#writer = Writer(fps=5, metadata=dict(artist='Me'), bitrate=1800)
 
 fig = plt.figure(figsize=(10,6))
-plt.xlim(0, 1000)
+plt.xlim(0, 1200)
 plt.ylim(np.min(plot)[0], np.max(plot)[0])
 plt.xlabel('Time',fontsize=20)
 plt.ylabel(title,fontsize=20)
@@ -36,6 +36,9 @@ def animate(i):
     p.tick_params(labelsize=17)
     plt.setp(p.lines,linewidth=1)
 
-ani = matplotlib.animation.FuncAnimation(fig, animate, frames=500, repeat=True)
+ani = matplotlib.animation.FuncAnimation(fig, animate, frames=1200,interval=1, repeat=True)
+#ani.save('lune.mp4', fps=17, dpi=100)
 
+writer = animation.FFMpegWriter(fps=70, metadata=dict(artist='Me'), bitrate=1800)
+ani.save("movie.mp4", writer=writer)
 plt.show()
