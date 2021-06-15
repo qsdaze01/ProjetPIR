0dc24860ad06fd7c1e4236dc4a74f7b844cf580d
Dvd-Luc <Dvd-Luc>
2021-06-15 17:49:59 +0200

    video demo au clair de la lune

--- Présentation/plot/plot.py
+++ Présentation/plot/plot.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
 
-x = range(1200)
+x = range(1000)
 val_read = [];
 with open(sys.argv[1], 'r') as f:
     d = f.readlines()
@@ -24,21 +24,21 @@ plot.columns = {title}
 #writer = Writer(fps=5, metadata=dict(artist='Me'), bitrate=1800)
 
 fig = plt.figure(figsize=(10,6))
-plt.xlim(0, 1200)
-plt.ylim(np.min(plot)[0], np.max(plot)[0])
+plt.xlim(0, 1000)
+plt.ylim(np.min(plot)[0], np.max(plot)[0]+10)
 plt.xlabel('Time',fontsize=20)
 plt.ylabel(title,fontsize=20)
 plt.title('Frequency',fontsize=20)
 
 def animate(i):
-    data = plot.iloc[:int(i+1)] #select data range
+    data = plot.iloc[:int(i+5)] #select data range
     p = sns.lineplot(x=data.index, y=data[title], data=data, color="r")
     p.tick_params(labelsize=17)
-    plt.setp(p.lines,linewidth=1)
+    plt.setp(p.lines,linewidth=0.2)
 
 ani = matplotlib.animation.FuncAnimation(fig, animate, frames=1200,interval=1, repeat=True)
-#ani.save('lune.mp4', fps=17, dpi=100)
+ani.save('lune2.mp4', fps=24, dpi=200)
 
-writer = animation.FFMpegWriter(fps=70, metadata=dict(artist='Me'), bitrate=1800)
-ani.save("movie.mp4", writer=writer)
-plt.show()
+#writer = animation.FFMpegWriter(fps=70, metadata=dict(artist='Me'), bitrate=1800)
+#ani.save("movie.mp4", writer=writer)
+#plt.show()
